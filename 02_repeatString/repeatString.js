afe2ef66f29740f1855e9bbc4a0f920ae777a1a4
Richard Goldsmith <rgolds33703@gmail.com>
2022-01-14 23:34:06 -0500

    completed exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+ const repeatString = function(string, num) {
+    myString = '';
+    if (num < 0) {
+        return 'ERROR'
+    }
+    while (num > 0) {
+        myString+=string
+        num--
+    }        
+    return myString;
+    
 };
 
 // Do not edit below this line
