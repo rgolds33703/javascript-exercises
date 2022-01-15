fb585e6214f85faf1ff862cfc1573fcf59591773
Richard Goldsmith <rgolds33703@gmail.com>
2022-01-15 00:56:04 -0500

    completed exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let sum=0;
+    if (min > max) {
+        let x = min;   //i know there is a better way to do this...just have to figure it out
+        min = max;
+        max = x;
+    }
+    if ((min < 0 || !Number.isInteger(min)) || (max < 0 || !Number.isInteger(max))) {
+        return 'ERROR'
+    } else {
+        for (i=min; i<=max; i++) {
+        sum+=i
+    }
+    return sum;
+    }
 
 };
 
