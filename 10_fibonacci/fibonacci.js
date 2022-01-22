f07b336e44f4d17b4e46c7e6c0df83dc92f434f1
Richard Goldsmith <rgolds33703@gmail.com>
2022-01-22 00:27:00 -0500

    completed exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num < 0) {
+        return `OOPS`;
+    } else if  (num < 2) {
+        return num;
+    }
+    return fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
