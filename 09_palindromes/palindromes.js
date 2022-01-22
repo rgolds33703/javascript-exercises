f50c179de3981fd67f3516280196271e1edaec43
Richard Goldsmith <rgolds33703@gmail.com>
2022-01-21 22:05:52 -0500

    completed exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let charOnlyString = string.replace(/[^A-Z0-9]/ig, "").toLowerCase();
+    let revString = charOnlyString.split('').reverse().join('');
+    return (charOnlyString === revString);
 };
 
 // Do not edit below this line
