bbd8d04019e4cc7b1ad401ebd4f74e93935c62c4
Richard Goldsmith <rgolds33703@gmail.com>
2022-01-21 21:44:32 -0500

    completed exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let sum = 0;
+  nums.forEach(e => sum += e);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let product = 1;
+  nums.forEach(e => product *= e);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let array = [];
+  for (i=1; i<=num; i++) {
+    array.push(i);
+  }
+  let product = 1;
+  array.forEach(e => product *=e);
+  return product;
 };
 
 // Do not edit below this line
